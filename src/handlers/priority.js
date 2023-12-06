c4b5dcd33f35849aea996a55044dd6facb06f583
smyler3 <mylertalym01@gmail.com>
2023-12-07 05:05:04 +1100

    Add ENUMs for priority and status values
    Also removed 'handler' suffix from modules for clarity and brevity

---
+++ src/handlers/priority.js
@@ -0,0 +1,12 @@
+/* Priority ENUM */
+const Priority = {
+    HIGH: "High",
+    MED: "Med",
+    LOW: "Low",
+    NONE: "-",
+}
+
+/* Checks whether the priority value is valid */
+function isValidPriority(priority) {
+    return Object.values(Priority).includes(priority);
+}
---
+++ src/handlers/status.js
@@ -0,0 +1,11 @@
+/* Status ENUM */
+const Status = {
+    DONE: "Done",
+    DOING: "Doing",
+    TODO: "To Do",
+}
+
+/* Checks whether the status property is valid */
+function isValidStatus(status) {
+    return Object.values(Status).includes(status);
+}
