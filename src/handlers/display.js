39746cd0ff9153fc486631561027d41751d88fd0
smyler3 <mylertalym01@gmail.com>
2023-12-13 16:05:57 +1100

    Rename main module for all page directories
    This will increase clarity by indicating which module is exporting the
    function to render the page

--- src/handlers/display.js
+++ src/handlers/display.js
@@ -1,6 +1,6 @@
 import { renderSidebarProjects, addSidebarProject } from "../sidebar";
-import renderProjectPage from "../pages/projectPage/projectPage";
-import renderAllProjectsPage from "../pages/allProjectsPage/allProjectsPage";
+import renderProjectPage from "../pages/projectPage";
+import renderAllProjectsPage from "../pages/allProjectsPage";
 import { renderForm, renderCreateProjectForm, renderCreateTaskForm, renderCreateStepForm, renderDeleteForm, renderColourPickerForm } from "../forms/formGenerator";
 
 export { 
